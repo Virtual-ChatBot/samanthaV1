a5cf939ec828014fefd2dae28f2273ceac7ef187
BEEMIL <powerpoopoo@naver.com>
2023-07-17 02:35:05 +0900

    2023-07-17 02:34 Invalid character found in method name 에러 대처

--- src/main/java/com/beemil/samantha/SamanthaV1Application.java
+++ src/main/java/com/beemil/samantha/SamanthaV1Application.java
@@ -1,12 +1,16 @@
 package com.beemil.samantha;
 
+import com.beemil.samantha.config.web.TomcatConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(TomcatConfig.class)
 public class SamanthaV1Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SamanthaV1Application.class, args);
 	}
 }
