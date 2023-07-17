16ffd1ede219adaf00b05ef33eb03d7821b1907d
BEEMIL <powerpoopoo@naver.com>
2023-07-18 06:37:20 +0900

    2023-07-18 06:37 CommonController 생성(index 접속 활성화9)

--- src/main/java/com/beemil/samantha/SamanthaV1Application.java
+++ src/main/java/com/beemil/samantha/SamanthaV1Application.java
@@ -1,12 +1,10 @@
 package com.beemil.samantha;
 
-import com.beemil.samantha.config.web.TomcatConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(TomcatConfig.class)
 public class SamanthaV1Application {
 
 	public static void main(String[] args) {
