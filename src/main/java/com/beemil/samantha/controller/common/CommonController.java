502d82e7a9181dedd6b8c8de3b2fc51e23f3340e
BEEMIL <powerpoopoo@naver.com>
2023-07-17 10:44:54 +0900

    2023-07-17 10:44 CommonController 생성(index 접속 활성화4)

--- src/main/java/com/beemil/samantha/controller/common/CommonController.java
+++ src/main/java/com/beemil/samantha/controller/common/CommonController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CommonController {
 
-    @GetMapping("/")
-    public String index() {
-
-        System.out.println("::");
-        System.out.println("[CommonController] 비밀연구소 오신 걸 환영합니다");
-
-        return "index";
-    }
+//    @GetMapping("/")
+//    public String index() {
+//
+//        System.out.println("::");
+//        System.out.println("[CommonController] 비밀연구소 오신 걸 환영합니다");
+//
+//        return "index";
+//    }
 }
