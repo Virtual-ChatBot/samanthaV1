cada9adc8ece75e265e3bb1391baa17829d02351
BEEMIL <powerpoopoo@naver.com>
2023-07-17 19:51:19 +0900

    2023-07-17 19:50 CommonController 생성(index 접속 활성화6)

--- src/main/java/com/beemil/samantha/config/web/WebConfig.java
+++ src/main/java/com/beemil/samantha/config/web/WebConfig.java
@@ -1,32 +1,35 @@
 package com.beemil.samantha.config.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-	
-	@Autowired
-	private LoginCheckInterceptor loginCheckInterceptor;
-	
+
+	private final LoginCheckInterceptor loginCheckInterceptor;
+
+	public WebConfig(LoginCheckInterceptor loginCheckInterceptor) {
+
+		this.loginCheckInterceptor = loginCheckInterceptor;
+	}
+
 	@Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginCheckInterceptor)
-                .addPathPatterns("/**") // 모든 requestURL에 대해 진입 거부
-                .excludePathPatterns(	// 제외 대상
-                		  "/"
-                        , "/css/**"
-                        , "/fonts/**"
-                        , "/images/**"
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(loginCheckInterceptor)
+				.addPathPatterns("/**") // 모든 request URL 진입 거부
+				.excludePathPatterns(	// 제외 대상
+						"/"
+						, "/css/**"
+						, "/fonts/**"
+						, "/images/**"
 						, "/media/**"
-                        , "/js/**"
+						, "/js/**"
 						, "/bot/stt/**"
 						, "/bot/chat/**"
 						, "/bot/tts/**"
 						, "/bot/navi/**"
 						, "/user/json/loginCk/**"
 						, "/user/logout/**");
-    }
+	}
 }
