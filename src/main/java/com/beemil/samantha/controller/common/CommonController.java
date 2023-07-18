9219dc7a0a90b13bc235ae5c59acd07413498d91
BEEMIL <powerpoopoo@naver.com>
2023-07-18 12:17:16 +0900

    2023-07-18 06:37 CommonController 생성(index 접속 활성화10)

--- src/main/java/com/beemil/samantha/controller/common/CommonController.java
+++ src/main/java/com/beemil/samantha/controller/common/CommonController.java
@@ -2,11 +2,12 @@ package com.beemil.samantha.controller.common;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CommonController {
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String index() {
 
         System.out.println("::");
