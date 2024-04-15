fb8d17d6a17d989a226f85667eea558cf559cc7d
BEEMIL <powerpoopoo@naver.com>
2024-04-16 01:23:39 +0900

    SSH 터널링 접속 도구 추가

--- src/main/java/site/beemil/samantha/config/db/DatabaseConfig.java
+++ src/main/java/site/beemil/samantha/config/db/DatabaseConfig.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,9 +13,13 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import site.beemil.samantha.util.SshTunnelingUtil;
 
 @Configuration
 public class DatabaseConfig {
+
+	//@Autowired
+	//private SshTunnelingUtil sshTunnelingUtil;
 
 	@Bean
 	@ConfigurationProperties(prefix = "spring.datasource.hikari")
@@ -23,9 +28,10 @@ public class DatabaseConfig {
 	}
 
 	@Bean
-	public DataSource dataSource() {
-		return new HikariDataSource(hikariConfig());
+	public DataSource dataSource(HikariConfig hikariConfig) {
+		return new HikariDataSource(hikariConfig);
 	}
+
 
 	@Bean
 	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
