5144ae0194bb2f393558cda2c80fa22ecdd79084
BEEMIL <powerpoopoo@naver.com>
2024-04-17 15:52:33 +0900

    UserServiceImpl 추가

--- src/main/java/site/beemil/samanthaV1/service/UserService.java
+++ src/main/java/site/beemil/samanthaV1/service/UserService.java
@@ -1,26 +1,9 @@
 package site.beemil.samanthaV1.service;
 
-
-import site.beemil.samanthaV1.dao.UserDAO;
 import site.beemil.samanthaV1.vo.UserVO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-@Service
-public class UserService {
+public interface UserService {
 
-	///Field
-	private final UserDAO userDao;
-
-	///Constructor
-	@Autowired
-	public UserService(UserDAO userDao) {
-		System.out.println("::"+getClass()+".setUserRepository Call.........");
-		this.userDao = userDao;
-	}
-
-	public UserVO login(String userId) throws Exception {
-		return userDao.login(userId);
-	}
+	UserVO login(String userId) throws Exception;
 
 }
